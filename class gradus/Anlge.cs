2ff9a187cd8889781ff0e2ce0696aaa66630a455
vladspiridonov <vlad777.96@mail.ru>
2024-02-15 23:56:39 +0300

    Добавьте файлы проекта.

---
+++ class gradus/Anlge.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_gradus
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Gradus += value / 60;
+                sec = value % 60;
+            }
+        }
+        public Angle(int gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return Math.PI / 180 * gradus + Math.PI / 180 / 60 * min + Math.PI / 180 / 60 / 60 * sec;
+        }
+    }
+}
---
+++ class gradus/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_gradus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gradus: ");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Minute: ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second: ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gradus, min, sec);
+
+            double rad = angle.ToRadians();
+            Console.WriteLine(rad);
+            Console.ReadKey();
+        }
+    }
+}
